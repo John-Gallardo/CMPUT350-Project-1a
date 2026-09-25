57638c6183730d6bff9d8f8b3d4abe25abd0488e
Avishek Dangol <dangolavishek202@gmail.com>
2026-09-25 09:57:37 -0600

    Math Utilities Completed

--- engine/MathUtil.h
+++ engine/MathUtil.h
@@ -9,59 +9,61 @@ namespace CMPUT350 {
 struct Point2D {
     float x, y;
     Point2D(float x = 0, float y = 0) : x(x), y(y) {}
-    double Distance(const Point2D &other) const {
+    double Distance(const Point2D& other) const {
         // Distance formula
-        return std::sqrt((x - other.x) * (x - other.x) + (y - other.y) * (y - other.y));
+        float dx = x - other.x;
+        float dy = y - other.y;
+        return std::sqrt(dx * dx + dy * dy);
     }
-    Point2D operator+(const Point2D &other) const {
+    Point2D operator+(const Point2D& other) const {
         // Vector addition
         return {x + other.x, y + other.y};
     }
-    Point2D operator+(const float &other) const {
+    Point2D operator+(const float& other) const {
         // Vector addition with a float
         return {x + other, y + other};
     }
-    Point2D operator-(const Point2D &other) const {
+    Point2D operator-(const Point2D& other) const {
         // Vector subtraction
         return {x - other.x, y - other.y};
     }
-    Point2D operator-(const float &other) const {
+    Point2D operator-(const float& other) const {
         // Vector subtraction with a float
         return {x - other, y - other};
     }
-    Point2D operator*(const float &scalar) const {
+    Point2D operator*(const float& scalar) const {
         // Typical vector and scalar multiplication
         return {x * scalar, y * scalar};
     }
-    Point2D &operator+=(const float &scalar) {
+    Point2D& operator+=(const float& scalar) {
         // Vector addition with a float
         x += scalar;
         y += scalar;
         return *this;
     }
-    Point2D &operator+=(const Point2D &other) {
+    Point2D& operator+=(const Point2D& other) {
         // Vector addition
         x += other.x;
         y += other.y;
         return *this;
     }
-    Point2D &operator-=(const Point2D &other) {
+    Point2D& operator-=(const Point2D& other) {
         // Vector subtraction
         x -= other.x;
         y -= other.y;
         return *this;
     }
-    bool operator==(const Point2D &other) const {
+    bool operator==(const Point2D& other) const {
         // Components must be equal
         return x == other.x && y == other.y;
     }
-    Point2D &operator*=(const int &scalar) {
+    Point2D& operator*=(const int& scalar) {
         // Vector and scalar multiplication
         x *= scalar;
         y *= scalar;
         return *this;
     }
-    Point2D &operator/=(const int &scalar) {
+    Point2D& operator/=(const int& scalar) {
         // Vector and scalar division
         if (scalar == 0) {
             std::cerr << "Warning: scalar division by 0. x & y set to 0";
@@ -74,9 +76,10 @@ struct Point2D {
 
         return *this;
     }
-    float operator*(const Point2D &other) const {
+    float operator*(const Point2D& other) const {
         // Dot product
         return x * other.x + y * other.y;
+        // return Dot(other);
     }
     float Dot(Point2D b) const {
         // Dot product with object & b
@@ -92,7 +95,7 @@ struct Point2D {
     }
     void Normalize() {
         // Normalize by dividing each component (x, y) by length
-        float length = std::sqrt(x*x + y*y);
+        float length = std::sqrt(x * x + y * y);
         if (length == 0) {
             std::cerr << "Warning: length is 0";
         } else {
@@ -102,13 +105,13 @@ struct Point2D {
     }
 };
 
-static std::ostream &operator<<(std::ostream &os, const Point2D &p) {
+static std::ostream& operator<<(std::ostream& os, const Point2D& p) {
     // Format is "(x, y)"
-    std::cout << '(' << p.x << ", " << p.y << ')';
+    os << '(' << p.x << ", " << p.y << ')';
     return os;
 }
 
-static Point2D operator*(float number, const Point2D &rhs) {
+static Point2D operator*(float number, const Point2D& rhs) {
     return {number * rhs.x, number * rhs.y};
 }
 
@@ -117,22 +120,49 @@ struct Line {
 
     Line(Point2D p1 = {0, 0}, Point2D p2 = {0, 0}) : p1(p1), p2(p2) {}
     Line(float x1, float y1, float x2, float y2) : p1(x1, y1), p2(x2, y2) {}
-    float Length() const {
-        // TODO: write this code
-        return 0;
+
+    float Length() const { return static_cast<float> p1.Distance(p2); }
+
+    Point2D ClosestPoint(const Point2D& p) const {
+        Point2D d = p2 - p1;
+        float lengthSquared = d.Dot(d);
+        // If the line segment is a point, return p1
+        if (lengthSquared == 0) {
+            return p1;
+        }
+        // projecting the point onto the line and scaling it to the line segment
+        float t = (p - p1).Dot(d) / lengthSquared;
+        if (t < 0) {
+            t = 0;
+        } else if (t > 1) {
+            t = 1;
+        }
+        return p1 + d * t;
     }
-    Point2D ClosestPoint(const Point2D &p) const {
-        // TODO: write this code
-        return p;
-    }
-    bool Crosses(Line other, Point2D &crossingPoint) const {
-        // TODO: write this code
+
+    bool Crosses(Line other, Point2D& crossingPoint) const {
+        Point2D ab = p2 - p1;
+        Point2D xy = other.p2 - other.p1;
+        float linesCrossPdt = Point2D::Cross(ab, xy);
+        // checking for parallel lines, if the cross product is 0, then the lines are parallel and
+        // do not intersect
+        if (std::fabs(linesCrossPdt) < 1e-8f) {
+            return false;
+        }
+        Point2D qp = other.p1 - p1;
+
+        float t = Point2D::Cross(qp, xy) / linesCrossPdt;
+        float u = Point2D::Cross(qp, ab) / linesCrossPdt;
+        if (t >= 0 && t <= 1 && u >= 0 && u <= 1) {
+            crossingPoint = p1 + ab * t;
+            return true;
+        }
         return false;
     }
 };
 
-static std::ostream &operator<<(std::ostream &os, const Line &l) {
-    // TODO: write this code
+static std::ostream& operator<<(std::ostream& os, const Line& l) {
+    os << l.p1 << " -> " << l.p2;
     return os;
 }
 
@@ -163,41 +193,88 @@ struct Rect {
     Rect(Point2D center, float radius)
         : topLeft(center.x - radius, center.y - radius), width(2 * radius), height(2 * radius) {}
 
-    Rect &operator|=(const Rect &other) {
-        // TODO: write this code
+    Rect& operator|=(const Rect& other) {
+        // expand this rectangle to include the other rectangle
+        float left = std::min(topLeft.x, other.topLeft.x);
+        float top = std::min(topLeft.y, other.topLeft.y);
+        float right = std::max(topLeft.x + width, other.topLeft.x + other.width);
+        float bottom = std::max(topLeft.y + height, other.topLeft.y + other.height);
+        // Update the current rectangle to the new bounds
+        topLeft = Point2D(left, top);
+        width = right - left;
+        height = bottom - top;
         return *this;
     }
-    Rect &operator|=(const Point2D &other) {
-        // TODO: write this code
+    Rect& operator|=(const Point2D& other) {
+        float left = std::min(topLeft.x, other.x);
+        float top = std::min(topLeft.y, other.y);
+        float right = std::max(topLeft.x + width, other.x);
+        float bottom = std::max(topLeft.y + height, other.y);
+        // update the AABB to include the point
+        topLeft = Point2D(left, top);
+        width = right - left;
+        height = bottom - top;
         return *this;
     }
-    Rect &operator|=(const Line &other) {
-        // TODO: write this code
+    Rect& operator|=(const Line& other) {
+        // line is just two points, so we can use the point operator to expand the rectangle to
+        // include both endpoints of the line
+        *this |= other.p1;
+        *this |= other.p2;
         return *this;
     }
-    Rect &operator&=(const Rect &other) {
-        // TODO: write this code
+
+    Rect& operator&=(const Rect& other) {
+        // shrink this rectangle to the intersection of this rectangle and the other rectangle
+        float left = std::max(topLeft.x, other.topLeft.x);
+        float top = std::max(topLeft.y, other.topLeft.y);
+        float right = std::min(topLeft.x + width, other.topLeft.x + other.width);
+        float bottom = std::min(topLeft.y + height, other.topLeft.y + other.height);
+        // If the rectangles do not intersect, set width and height to 0
+        if (right < left || bottom < top) {
+            width = 0;
+            height = 0;
+
+        } else {
+            topLeft = Point2D(left, top);
+            width = right - left;
+            height = bottom - top;
+        }
         return *this;
     }
-    Rect &operator+=(const Point2D &other) {
-        // TODO: write this code
+
+    Rect& operator+=(const Point2D& other) {
+        // Move the same rectangle by the given point
+        topLeft += other;
         return *this;
     }
-    Rect operator+(const Point2D &other) const {
-        // TODO: write this code
-        return *this;
+
+    Rect operator+(const Point2D& other) const {
+        return Rect(topLeft.x + other.x, topLeft.y + other.y, width, height);
     }
+
     void Inset(int inset) {
-        // TODO: write this code
+        topLeft.x += inset;
+        topLeft.y += inset;
+        width -= 2 * inset;
+        height -= 2 * inset;
+        if (width < 0) {
+            width = 0;
+        }
+        if (height < 0) {
+            height = 0;
+        }
     }
-    bool IsInside(const Point2D &p) const {
-        // TODO: write this code
+
+    bool IsInside(const Point2D& p) const {
+        return p.x >= topLeft.x && p.x <= topLeft.x + width && p.y >= topLeft.y &&
+               p.y <= topLeft.y + height;
         return false;
     }
 };
 
-static std::ostream &operator<<(std::ostream &os, const Rect &l) {
-    // TODO: write this code
+static std::ostream& operator<<(std::ostream& os, const Rect& l) {
+    os << '[' << l.topLeft << ' ' << l.width << 'x' << l.height << ']';
     return os;
 }
 
